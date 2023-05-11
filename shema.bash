6c47e6ca664a763fdc540054f9b268a2c417916f
EliasSih <eliazsih@gmail.com>
2023-05-11 19:43:45 +0200

    deleted schema script and added metadata files

--- shema.bash
+++
@@ -1,7 +0,0 @@
-curl -X POST -H 'Content-type:application/json' --data-binary '{
-  "add-field":{
-     "name":"identifier",
-     "type":"text_general",
-     "stored":true }
-}' http://localhost:8983/solr/#/ETDSearch/schema
-
